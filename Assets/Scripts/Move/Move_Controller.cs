a947956726ef4d8692cc580780b646dbc97bb2f6
hshjbbnd <2412198819@qq.com>
2025-10-02 00:04:05 +0800

    0.0.1 Update
    使用Rigidbody移动代替RectTransform移动
    跳跃逻辑初步实现（感谢 Ren）
    暂时就这么多

--- Assets/Scripts/Move/Move_Controller.cs
+++ Assets/Scripts/Move/Move_Controller.cs
@@ -15,11 +15,29 @@ public class Move_Controller : MonoBehaviour
 
     public bool Effective_Move;//判断当前移动是否有效
 
+    public bool Effective_W;
+    public bool Effective_A;
+    public bool Effective_S;
+    public bool Effective_D;
     public bool Gravity_On;//是否使用重力
     // Start is called before the first frame update
+
+    public float Accelerate = 0f;
+    public float Speed = 0f;
+
+    public bool On_Base;//是否有地面作为支持
+
+
+
+
+
+
     void Start()
     {
-
+        Effective_W = true;
+        Effective_A=true;
+        Effective_S = true;
+        Effective_D = true;
         Debug.Log("Start from Move_Controller.cs");
         Current_Character = gameObject;
         Current_Transform = Current_Character.GetComponent<Transform>();
@@ -42,18 +60,19 @@ public class Move_Controller : MonoBehaviour
 
         Current_Transform.eulerAngles = new Vector3(0f, 0f, 0f);
 
+        /*
         if(Effective_Move==false)
         {
             Current_Transform.position = Post_Position;
             
             return;
         }
-
+        */
         Post_Position = new Vector3(Current_Transform.position.x, Current_Transform.position.y, Current_Transform.position.z);
 
 
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)&&Effective_W)
         {
 
             float New_X = Current_Transform.position.x;
@@ -63,7 +82,7 @@ public class Move_Controller : MonoBehaviour
             Current_Transform.position = new Vector3(New_X, New_Y,New_Z);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)&&Effective_S)
         {
 
             float New_X = Current_Transform.position.x;
@@ -73,7 +92,7 @@ public class Move_Controller : MonoBehaviour
             Current_Transform.position = new Vector3(New_X, New_Y, New_Z);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)&&Effective_A)
         {
 
             float New_X = Current_Transform.position.x- 0.05f;
@@ -83,7 +102,7 @@ public class Move_Controller : MonoBehaviour
             Current_Transform.position = new Vector3(New_X, New_Y, New_Z);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)&&Effective_D)
         {
 
             float New_X = Current_Transform.position.x + 0.05f;
@@ -100,7 +119,55 @@ public class Move_Controller : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"触发器触发！与 {other.gameObject.name} 碰撞");
-        Effective_Move = false;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            Effective_W = false;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Effective_A = false;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Effective_S = false;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Effective_D = false;
+        }
+
+        //Effective_Move = false;
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(Effective_W==false)
+        {
+            Effective_W = true;
+            //return;
+        }
+
+        if (Effective_A == false)
+        {
+            Effective_A = true;
+            //return;
+        }
+
+        if (Effective_S == false)
+        {
+            Effective_S = true;
+            //return;
+        }
+
+        if (Effective_D == false)
+        {
+            Effective_D = true;
+            //return;
+        }
     }
 
     public void Shift_Mode()
@@ -110,15 +177,18 @@ public class Move_Controller : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.F))
         {
-            if(Gravity_On==false)
+            if(rb.IsAwake())
             {
-                Gravity_On = true;
-                rb.gravityScale = 1f;
+                rb.Sleep();
+                //Debug.Log("物体已进入休眠状态");
+                //Gravity_On = true;
+                //rb.gravityScale = 1f;
             }
             else
             {
-                Gravity_On = false;
-                rb.gravityScale = 0f;
+                rb.WakeUp();
+                //Gravity_On = false;
+                //rb.gravityScale = 0f;
             }
         }
     }
---
+++ Assets/Scripts/Move/Player.cs
@@ -0,0 +1,244 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject Current_Character;
+
+
+    public Transform Current_Transform;
+
+    public Vector3 Post_Position;
+
+    public bool Effective_Move;//判断当前移动是否有效
+
+    public bool Effective_W;
+    public bool Effective_A;
+    public bool Effective_S;
+    public bool Effective_D;
+    public bool Gravity_On;//是否使用重力
+
+    // Start is called before the first frame update
+
+    public float Accelerate = 0f;
+    public float Speed = 0f;
+
+    public bool On_Ground;//是否有地面作为支持
+
+    public float jumpForce = 1f;
+    public bool isJumping;
+    public float jumpTimeCounter;
+
+    void Start()
+    {
+        Effective_W = true;
+        Effective_A = true;
+        Effective_S = true;
+        Effective_D = true;
+        Debug.Log("Start from Move_Controller.cs");
+        Current_Character = gameObject;
+        Current_Transform = Current_Character.GetComponent<Transform>();
+        Post_Position = new Vector3(Current_Transform.position.x, Current_Transform.position.y, Current_Transform.position.z);
+
+
+        Effective_Move = true;
+        Gravity_On = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        CheckGrounded();
+        if (Gravity_On)
+        {
+            Jump(rb);
+        }
+        Move(rb);
+        Shift_Mode(rb);
+        
+    }
+
+    public void Move(Rigidbody2D rb)
+    {
+
+        //Current_Transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+        /*
+        if(Effective_Move==false)
+        {
+            Current_Transform.position = Post_Position;
+            
+            return;
+        }
+        */
+        Post_Position = new Vector2(Current_Transform.position.x, Current_Transform.position.y);
+
+        Vector2 New_Pos=new Vector2(Post_Position.x,Post_Position.y);
+
+        if (!Gravity_On)
+        {
+            if (Input.GetKey(KeyCode.W) && Effective_W)
+            {
+
+                float New_X = Current_Transform.position.x;
+                float New_Y = Current_Transform.position.y + 0.05f;
+                
+
+                New_Pos = new Vector2(New_X, New_Y);
+            }
+
+            if (Input.GetKey(KeyCode.S) && Effective_S)
+            {
+
+                float New_X = Current_Transform.position.x;
+                float New_Y = Current_Transform.position.y - 0.05f;
+                
+
+                New_Pos = new Vector2(New_X, New_Y);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A) && Effective_A)
+        {
+
+            float New_X = Current_Transform.position.x - 0.05f;
+            float New_Y = Current_Transform.position.y;
+            
+
+            New_Pos = new Vector2(New_X, New_Y);
+        }
+
+        if (Input.GetKey(KeyCode.D) && Effective_D)
+        {
+
+            float New_X = Current_Transform.position.x + 0.05f;
+            float New_Y = Current_Transform.position.y;
+            
+
+            New_Pos = new Vector2(New_X, New_Y);
+        }
+
+        rb.position=new Vector2(New_Pos.x,New_Pos.y);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"触发器触发！与 {other.gameObject.name} 碰撞");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Effective_W = false;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Effective_A = false;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Effective_S = false;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Effective_D = false;
+        }
+
+        //Effective_Move = false;
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (Effective_W == false)
+        {
+            Effective_W = true;
+            //return;
+        }
+
+        if (Effective_A == false)
+        {
+            Effective_A = true;
+            //return;
+        }
+
+        if (Effective_S == false)
+        {
+            Effective_S = true;
+            //return;
+        }
+
+        if (Effective_D == false)
+        {
+            Effective_D = true;
+            //return;
+        }
+    }
+
+    public void Shift_Mode(Rigidbody2D rb)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!Gravity_On)
+            {
+                Gravity_On = true;
+                rb.gravityScale = 1f;
+            }
+            else
+            {
+                Gravity_On = false;
+                rb.gravityScale = 0f;
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+            }
+        }
+    }
+
+    void CheckGrounded()
+    {
+        // 这里实现的地面检测逻辑
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f);
+        if (hit.collider != null)
+        {
+            On_Ground = true;
+        }
+        else 
+        { 
+            On_Ground = false;
+        }
+    }
+
+    void Jump(Rigidbody2D rb)
+    {
+        float jumpTime = 0.5f;//跳跃按键持续最大时间
+ 
+        // 按下跳跃键
+        if (Input.GetKeyDown(KeyCode.W) && On_Ground)
+        {
+            jumpTimeCounter = jumpTime;
+            isJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        // 按住跳跃键
+        if (Input.GetKey(KeyCode.W) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        // 松开跳跃键
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isJumping = false;
+        }
+    }
+}
