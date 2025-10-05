12eb5c4eec1b8a576bb1bb461daa08c563a86bc3
hshjbbnd <2412198819@qq.com>
2025-10-05 17:11:40 +0800

    将接地检测独立于Check_Ground.cs的grounded变量中

--- Assets/Scripts/Move/Player.cs
+++ Assets/Scripts/Move/Player.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject Current_Character;
-
-
     public Transform Current_Transform;
-
     public Vector3 Post_Position;
 
     public bool Effective_Move;//判断当前移动是否有效
-
     public bool Effective_W;
     public bool Effective_A;
     public bool Effective_S;
@@ -21,21 +17,22 @@ public class Player : MonoBehaviour
 
     // Start is called before the first frame update
 
-    public bool On_Ground;//是否有地面作为支持
+    //public bool On_Ground;//是否有地面作为支持
 
     public float jumpForce = 0.01f;//跳跃系数
     public bool isJumping;
     public float jumpTimeCounter;
 
     private float Move_Speed=0.1f;//移动速度系数
-
     private float Gravity_Scale = 2.25f;//重力系数
-
     private float jumpTime = 0.5f;//跳跃按键持续最大时间
+    private Check_Ground groundChecker;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        groundChecker = gameObject.GetComponent<Check_Ground>();
         Effective_W = true;
         Effective_A = true;
         Effective_S = true;
@@ -55,18 +52,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-        CheckGrounded();
         if (Gravity_On)
         {
-            Jump(rb);
+            Jump();
         }
-        Move(rb);
-        Shift_Mode(rb);
+        Move();
+        Shift_Mode();
         
     }
 
-    public void Move(Rigidbody2D rb)
+    public void Move()
     {
 
         //Current_Transform.eulerAngles = new Vector3(0f, 0f, 0f);
@@ -127,7 +122,7 @@ public class Player : MonoBehaviour
       
     }
 
-    public void Shift_Mode(Rigidbody2D rb)
+    public void Shift_Mode()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
@@ -149,31 +144,10 @@ public class Player : MonoBehaviour
         }
     }
 
-    void CheckGrounded()
-    {
-        // 这里实现的地面检测逻辑
-        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 1f);
-        if (hits.Length > 0)
-        {
-            foreach (RaycastHit2D hit in hits)
-            {
-                // 遍历每一个被击中的物体
-                //Debug.Log($"击中了物体：{hit.collider.gameObject.name}，距离是：{hit.distance}");
-                if (hit.collider.gameObject == gameObject)
-                {
-                    continue;
-                }
-                On_Ground = true;
-                return;
-            }
-        }
-        On_Ground = false;
-    }
-
-    void Jump(Rigidbody2D rb)
+    void Jump()
     { 
         // 按下跳跃键
-        if (Input.GetKeyDown(KeyCode.W) && On_Ground)
+        if (Input.GetKeyDown(KeyCode.W) && groundChecker.grounded)
         {
             jumpTimeCounter = jumpTime;
             isJumping = true;
