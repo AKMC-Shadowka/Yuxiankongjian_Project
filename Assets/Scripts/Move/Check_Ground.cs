a77f0bbbbcca4c929162f21b5da68a9f15ca712f
AKMC-Jueying <1484237624@qq.com>
2025-10-10 22:28:13 +0800

    Update Level_1

--- Assets/Scripts/Move/Check_Ground.cs
+++ Assets/Scripts/Move/Check_Ground.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Check_Ground : MonoBehaviour
 {
+    public bool grounded;
     // Start is called before the first frame update
     void Start()
     {
-        
+        grounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 这里实现的地面检测逻辑
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 1f);
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit2D hit in hits)
+            {
+                // 遍历每一个被击中的物体
+                //Debug.Log($"击中了物体：{hit.collider.gameObject.name}，距离是：{hit.distance}");
+                if (hit.collider.gameObject != gameObject)
+                {
+                    grounded = true;
+                    break;
+                }
+                grounded = false;
+            }
+        }
     }
 }
